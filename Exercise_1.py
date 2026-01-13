4c845dc5e9c72a3faa74fd91330d1cd46f9f3dcc
varnika-98 <varnikaprasad98@gmail.com>
2026-01-12 16:31:18 -0800

    Completed Exercise 1: Stack implementation

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,21 +1,29 @@
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
+  #Time Complexity: push: O(1), pop: O(1), peek: O(1), isEmpty: O(1), size: O(1), show: O(n)
+  #Space Complexity: O(n)
+
      def __init__(self):
+         self.items = []
          
      def isEmpty(self):
+         return len(self.items) == 0
          
      def push(self, item):
+         self.items.append(item)
          
      def pop(self):
-        
-        
+         if not self.isEmpty():
+             return self.items.pop()
+         
      def peek(self):
-        
+         if not self.isEmpty():
+             return self.items[-1]
+         
      def size(self):
+         return len(self.items)
          
      def show(self):
-         
+         print(self.items)
 
 s = myStack()
 s.push('1')
